11fa3366ea516a81d2f06d0e9835034eefc2d4a2
Jalba Cosmin <cosjin35@gmail.com>
2025-07-04 11:09:42 +0300

    created service class

--- Backend/CLI-Reddit/src/Main.java
+++ Backend/CLI-Reddit/src/Main.java
@@ -1,20 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Espresso");
 
-        try {
-            Connection conn = DB.getConnection();
-            System.out.println("Database connection established!");
-        } catch (SQLException e) {
-            System.err.println("Database connection failed: " + e.getMessage());
+        Scanner scanner = new Scanner(System.in);
+
+        Service service = new Service();
+        while (true) {
+
+            System.out.println("Ce doriti sa faceti:");
+            System.out.println("1. Login");
+            System.out.println("2. Register");
+            System.out.println("3. Create a post");
+            System.out.println("4. Show a post");
+            System.out.println("5. Delete a post");
+            System.out.println("6. Add a comment to a post");
+            System.out.println("7. Add a comment to a comment");
+            System.out.println("8. Vote a post");
+            System.out.println("9. Vote a comment");
+            System.out.println("10. Exit");
+
+            try {
+                int option = scanner.nextInt();
+                scanner.nextLine();
+                switch (option) {
+                    case 1:
+                        service.login(scanner);
+                        break;
+                    case 2:
+                        service.register(scanner);
+                        break;
+                    case 3:
+                        service.createPost(scanner);
+                        break;
+                    case 4:
+                        service.showPost(scanner);
+                        break;
+                    case 5:
+                        service.deletePost(scanner);
+                        break;
+                    case 6:
+                        service.addCommentToPost(scanner);
+                        break;
+                    case 7:
+                        service.addCommentToComment(scanner);
+                        break;
+                    case 8:
+                        service.addVoteToPost(scanner);
+                        break;
+                    case 9:
+                        service.addVoteToComment(scanner);
+                        break;
+                    case 10:
+                        System.out.println("Exiting.");
+                        scanner.close();
+                        break;
+                    default:
+                        System.out.println("invalid option. try again");
+                        break;
+                }
+            } catch (NoSuchElementException e) {
+                System.out.println("Error: Please enter a valid integer option.");
+//                scanner.nextLine(); // Curăță inputul invalid
+
+            }
         }
-
     }
 }
---
+++ Backend/CLI-Reddit/src/Service.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Service {
+
+    public void login(Scanner scanner)
+    {
+
+    }
+    public void register(Scanner scanner)
+    {
+
+    }
+    public void createPost(Scanner scanner)
+    {
+
+    }
+    public void showPost(Scanner scanner)
+    {
+
+    }
+    public void deletePost(Scanner scanner)
+    {
+
+    }
+    public void addCommentToPost(Scanner scanner)
+    {
+
+    }
+    public void addCommentToComment(Scanner scanner)
+    {
+
+    }
+    public void addVoteToPost(Scanner scanner)
+    {
+
+    }
+    public void addVoteToComment(Scanner scanner)
+    {
+
+    }
+
+
+}
