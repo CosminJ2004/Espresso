8704e87f2b6a843c349a1f8a6ecc1902ff065560
Jalba Cosmin <cosjin35@gmail.com>
2025-07-04 11:28:03 +0300

    created Comment first version

---
+++ Backend/CLI-Reddit/src/Comment.java
@@ -0,0 +1,14 @@
+public abstract class Comment {
+    private User user;
+    private String textComment;
+
+    public Comment(User user, String textComment)
+    {
+        this.user=user;
+        this.textComment=textComment;
+    }
+
+
+
+
+}
---
+++ Backend/CLI-Reddit/src/CommentPost.java
@@ -0,0 +1,21 @@
+public class CommentPost extends Comment {
+    private Post post;
+
+    public CommentPost(User user, String textComment,Post post)
+    {
+        super(user,textComment);
+        this.post=post;
+    }
+    public void setTextComment(String textComment) {
+        this.textComment = textComment;
+    }
+
+    public String getTextComment() {
+        return textComment;
+    }
+    public void showComment()
+    {
+        System.out.println(user.username);
+        System.out.println(textComment);
+    }
+}
