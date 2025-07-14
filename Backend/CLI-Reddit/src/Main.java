9a4ec5f0c06cab8fe6c7cd2b57406e65f9dd0233
Marius-Augustin <marius.august.nitu@proton.me>
2025-07-14 18:56:28 +0300

    updated Log and Console usage

--- Backend/CLI-Reddit/src/Main.java
+++ Backend/CLI-Reddit/src/Main.java
@@ -2,14 +2,17 @@ import logger.FileLogger;
 import logger.Log;
 import logger.LogLevel;
 import util.Console;
+import util.DB;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*
+
         Service service = new Service();
         Menu menu = new Menu(service);
         DB.CheckDBDriver();
-        */
+
         Log.RegisterLogger(new FileLogger(LogLevel.DEBUG,"DebugLogs.txt"));
         Log.RegisterLogger(new FileLogger(LogLevel.ERROR, "ErrorsLogs.txt"));
 
@@ -17,9 +20,9 @@ public class Main {
         Log.log(LogLevel.WARN,"Debug Message");
         Log.log(LogLevel.ERROR,"Error Message");
 
-        /*while (true) {
+        while (true) {
             menu.createLoginMenu();
             menu.createMainMenu();
-        }*/
+        }
     }
 }
