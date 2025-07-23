7fc667094da579d6b6890ec0fec08cc89e36d9ad
Puiu Iustin <puiustin@gmail.com>
2025-07-23 15:42:44 +0300

    Made all repositories

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
-
     boolean existsByUsername(String username);
 
     Optional<Users> findByUsernameAndPassword(String username, String password);
