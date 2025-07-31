8ac7dfa30d4306c2de2bf4966c96a1017dd53646
Puiu Iustin <puiustin@gmail.com>
2025-07-31 12:40:05 +0300

    Fix compilation failure: package org.modelmapper does not exist

--- backend/src/main/java/com/example/backend/config/ApplicationConfig.java
+++ backend/src/main/java/com/example/backend/config/ApplicationConfig.java
@@ -1,17 +1,10 @@
 package com.example.backend.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.modelmapper.ModelMapper;
 
 @Configuration
 public class ApplicationConfig {
