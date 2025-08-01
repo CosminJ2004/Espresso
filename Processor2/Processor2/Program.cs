2ac83fb58fbc6cf9db0f13d95e589facfbf54bfc
Jalba Cosmin <cosjin35@gmail.com>
2025-08-01 14:02:51 +0300

    added fillter handler and DI

--- Processor2/Processor2/Program.cs
+++ Processor2/Processor2/Program.cs
@@ -1,56 +1,104 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.Extensions.Hosting;
-using Processing.Models;
 using Processing.Filters;
+using Processing.Interfaces;
+using Processing.Models;
 using Processing.Utils;
+using System.ComponentModel;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        RgbImageReader rgbImageReader = new RgbImageReader();
-        RgbImageWriter rgbImageWriter = new RgbImageWriter();
+
         var builder = WebApplication.CreateBuilder(args);
+
+
+        //   CreateBuilder(args)
+        //        ↓
+        //   Configure Services, Logging, Config, DI
+        //        ↓
+        //      Build()
+        //        ↓
+        //   Compose Middleware, Routes, DI container
+        //        ↓
+        //       Run()
+        //        ↓
+        //   Start Kestrel(server)
+        //        ↓
+        //   Accept HTTP requests + execute mapped endpoints
+
+
+        // builder.service -> access to the DI services 
+
+        // register components
+        builder.Services.AddScoped<RgbImageReader>();
+        builder.Services.AddScoped<RgbImageWriter>();
+        builder.Services.AddScoped<FilterHandler>();
+
+
+        builder.Services.AddScoped<IImageFilter, GrayscaleFilter>();
+        builder.Services.AddScoped<IImageFilter, InvertFilter>();
+
+
         var app = builder.Build();
         app.MapGet("/test", () => "OK");
 
-
-        app.MapPost("/filter", async (HttpRequest request) =>
-        {
-            var filterName = request.Query["filter"].ToString();
-            if (string.IsNullOrEmpty(filterName))
-                return Results.BadRequest("Missing filter name.");
-
-            var form = await request.ReadFormAsync();
-            var file = form.Files["image"];
-            if (file == null)
-                return Results.BadRequest("Missing image file.");
-
-            using var stream = file.OpenReadStream();
-            using var ms = new MemoryStream();
-            await stream.CopyToAsync(ms);
-            ms.Position = 0;
-
-            var inputImage = rgbImageReader.ReadFromStream(ms);
-
-
-            var outputImage = filterName.ToLower() switch
+        // Map the filter endpoint
+        // This endpoint expects a form-data request with an image file and a filter name
+        //dependency injection  for reading and writing images, as well as applying filters
+        app.MapPost("/filter", async (
+            HttpContext context,
+            RgbImageReader reader,
+            RgbImageWriter writer,
+            FilterHandler handler) =>
+                {
+            try
             {
-                "invert" => new InvertFilter().Apply(inputImage),
+                var request = context.Request;
+                var filterName = request.Query["filter"].ToString();
+                if (string.IsNullOrEmpty(filterName))
+                    return Results.BadRequest("Missing filter name");
 
-                "grayscale" => new GrayscaleFilter().Apply(inputImage),
-                _ => inputImage
+                if (!request.HasFormContentType)
+                    return Results.BadRequest("Content-Type must be form-data.");
 
-            };
+                var form = await request.ReadFormAsync();
+                var file = form.Files["image"];
+                if (file == null)
+                    return Results.BadRequest("Missing image");
 
-            var outStream = new MemoryStream();
-            rgbImageWriter.WriteToStream(outStream,outputImage);
-            outStream.Position = 0;
+                using var stream = file.OpenReadStream();
+                using var ms = new MemoryStream();
+                await stream.CopyToAsync(ms);
+                ms.Position = 0;
 
-            return Results.File(outStream, "image/jpeg");
+                var inputImage = reader.ReadFromStream(ms);
+
+                
+                var outputImage = handler.Apply(filterName, inputImage);
+
+                var outStream = new MemoryStream();
+                writer.WriteToStream(outStream, outputImage);
+                outStream.Position = 0;
+
+                return Results.File(outStream, "image/jpeg");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Results.BadRequest(new { error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+              
+                return Results.Problem(ex.Message+ " "+ex.StackTrace);
+            }
         });
-        app.Run("http://0.0.0.0:80");   
+        app.Run("http://0.0.0.0:80");
+
 
     }
 }
